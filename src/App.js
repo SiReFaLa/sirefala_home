79a7f529f0de39fe111872d9f90a2d8e3453095f
kontate <tatekichixe@gmail.com>
2023-03-03 01:21:46 +0900

    contents scribble

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 
+const members = [
+  ["ななみつき","nanamitsuki_73"],
+  ["Urban","aaaa"]
+]
+
 function App() {
   return (
     <div className="App">
       <h1>しれふぁら！！</h1>
+      ボーマス51に申し込み中。
+
+      <h2>メンバー</h2>
+      {members.map((it)=><p>
+        <a>・ {it[0]} </a> 
+        <a href={'https://twitter.com/'+it[1]}>
+          @{it[1]}
+        </a>
+      </p>)}
+
+      <h2>アルバム</h2>
+        
     </div>
   );
 }
