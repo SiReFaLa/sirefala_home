020cbedcccd3a79185e90bccc855f535d3f5683f
kontate <tatekichixe@gmail.com>
2023-03-03 15:35:32 +0900

    href error fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 
-// const members = [
-//   ["ななみつき","nanamitsuki_73"],
-//   ["Urban","aaaa"]
-// ]
+const members = [
+  ["ななみつき","nanamitsuki_73"],
+  ["Urban","aaaa"]
+]
 
 function App() {
   return (
@@ -12,6 +12,13 @@ function App() {
       ボーマス51に申し込み中。
 
       <h2>メンバー</h2>
+      {members.map((it, idx)=>
+      <p>
+        <a href={() => false} >・ {it[0]} </a> 
+        <a href = {'https://twitter.com/'+it[1]} >
+          @{it[1]}
+        </a>
+      </p>)}
 
       <h2>アルバム</h2>
 
