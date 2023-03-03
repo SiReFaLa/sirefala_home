1212418f0758fde16a30a013a072f51b5c22d97f
kontate <tatekichixe@gmail.com>
2023-03-03 15:07:50 +0900

    ugokan test

--- src/App.js
+++ src/App.js
@@ -12,13 +12,6 @@ function App() {
       ボーマス51に申し込み中。
 
       <h2>メンバー</h2>
-      {members.map((it)=>
-      <p>
-        <a>・ {it[0]} </a> 
-        <a href = {'https://twitter.com/'+it[1]}>
-          @{it[1]}
-        </a>
-      </p>)}
 
       <h2>アルバム</h2>
 
