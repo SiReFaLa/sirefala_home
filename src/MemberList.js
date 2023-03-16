be9ef1ec1686d490326333315f4c4f165a41dbc0
kontate <tatekichixe@gmail.com>
2023-03-16 20:14:15 +0900

    imgタグにalt=を追加

--- src/MemberList.js
+++ src/MemberList.js
@@ -39,7 +39,7 @@ function MemberItem(){
             {ShowSNSIcon(it)}
           </div>
           {it.comment}
-          {it.imgLink ? <img src={it.imgLink} width="100" height="100"/> : ""}
+          {it.imgLink ? <img alt = "" src={it.imgLink} width="100" height="100"/> : ""}
         </div>
       )
     )
