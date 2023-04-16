972f861f2222e1b418bf31cedeee367c9154e67a
kontate <tatekichixe@gmail.com>
2023-04-16 20:06:09 +0900

    fex>>fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-//const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
-urlPrefix = "/sirefala_home"
+const urlPrefix = process.env.NEXT_PUBLIC_URL_PREFIX ? '/' + process.env.NEXT_PUBLIC_URL_PREFIX : ''
+//urlPrefix = "/sirefala_home"
 
 module.exports = {
   assetPrefix: urlPrefix,
