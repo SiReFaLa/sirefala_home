f3b67092b837efd13e2ee762c9ea26bd94a0f43d
kontate <tatekichixe@gmail.com>
2023-04-16 19:49:09 +0900

    うおうお

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
   reactStrictMode: true,
+  trailingSlash: true
 }
 
 module.exports = nextConfig
+
