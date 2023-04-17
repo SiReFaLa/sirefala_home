4e8df1ceb3302f3fdbebf9832c53ee379c006eb5
kontate <tatekichixe@gmail.com>
2023-04-18 01:51:49 +0900

    セキュリティ的に良くないかもだけど一旦NEXT_PUBLICでKEY出して動くか見る

--- libs/microcms.ts
+++ libs/microcms.ts
@@ -39,15 +39,15 @@ export const getMemberDetail = async (
 };
 
 if (!process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN) {
-    //throw new Error("NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN is required");
+    throw new Error("NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN is required");
    }
    
-if (!process.env.MICROCMS_API_KEY) {
-    //throw new Error("MICROCMS_API_KEY is required");
+if (!process.env.NEXT_PUBLIC_MICROCMS_API_KEY) {
+    throw new Error("NEXT_PUBLIC_MICROCMS_API_KEY is required");
 }
 
 // API取得用のクライアントを作成
 export const client = createClient({
     serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN,
-    apiKey: process.env.MICROCMS_API_KEY,
+    apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY,
    });
