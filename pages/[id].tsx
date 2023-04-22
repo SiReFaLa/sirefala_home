6a3f1790e5373c8c8b790423df87a036545a131d
kontate <tatekichixe@gmail.com>
2023-04-23 01:09:25 +0900

    詳細ページのスタイルを最低限整える

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,22 +1,20 @@
 import { client, Page } from "../libs/microcms";
 import { GetStaticProps, GetStaticPaths } from "next";
 import { ParsedUrlQuery } from "querystring";
+import MyHead from "../components/MyHead";
 
-interface Props {
-    page: Page
-}
+interface Props {page: Page}
 
-interface Params extends ParsedUrlQuery {
-    id: string
-}
+interface Params extends ParsedUrlQuery {id: string}
 
 export default function PageId( {page}:Props) {
     return (
         <div className="App">
+            <MyHead siteName={page.title} description={page.abstruct}/>
             <h1 className="ContentItem">{page.title}</h1>
-            <div className="ContentItem"
+            <div className="prose text-black pt-7"
                 dangerouslySetInnerHTML={{
-                __html: `${page.text}`,
+                    __html: `${page.text}`,
                 }}
             />
         </div>
